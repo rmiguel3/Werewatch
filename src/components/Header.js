d6d1bbaf4c68eabdb493e3523dee980b68d91d22
rmiguel3 <47339252+rmiguel3@users.noreply.github.com>
2025-07-18 15:55:13 -0500

    Added logo to header and set up for api calls

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,17 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="text-4x mb-8 bg-gray-500 text-white p-4 rounded-full shadow-lg">
+    <header className="text-4x mb-8 bg-gray-500 text-white p-4 rounded-full shadow-xl">
         <nav className="flex p-4 space-x-20">
-            <a className="font-bold text-2xl" href="/">Home</a>
-            <a className="font-bold text-2xl" href="/about">Genres</a>
-            <a className="font-bold text-2xl" href="/services">Services</a>
-            <a className="font-bold text-2xl" href="/contact">Contact</a>
+          <img src="./logo.png" alt="" className="h-10 w-96 object-cover" ></img>
+          {[
+            ['Home', '/homePage'],
+            ['Genres', '/genres'],
+            ['About', '/about'],
+            ['Contact', '/contact'],
+          ].map(([title, url]) => (
+            <a href={url} className="font-bold text-2xl">{title}</a>
+          ))}
         </nav>
     </header>
   );
